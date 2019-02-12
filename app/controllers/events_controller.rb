91e59141c0f143da9a954ebe3e40eea555c8bac0
Montblanc159 <guillaume.zaugg@gmail.com>
2019-02-12 20:46:54 +0100

    Working create event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,25 @@ class EventsController < ApplicationController
 		@creator = @event.user
 		@participants = @event.users
 	end
+
+	def create
+		@event = Event.new(title: params[:title], description: params[:description], start_date: params[:start_date], duration: params[:duration], location: params[:location], price: params[:price], user: current_user)
+		puts @event.user
+		puts @event.title
+		puts @event.description
+		puts @event.start_date
+		puts @event.duration
+		puts @event.location
+		puts @event.price
+
+		if @event.save
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def event_params
+		params.permit(:title, :description, :start_date, :duration, :location)
+	end
 end
