9ffa53011e73c40fd34bfd0423f6444654ee9c4c
Salomé Marzin <salome.mrzn@gmail.com>
2019-02-12 16:54:46 +0100

    remove column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_11_150659) do
+ActiveRecord::Schema.define(version: 2019_02_12_155336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,15 +32,11 @@ ActiveRecord::Schema.define(version: 2019_02_11_150659) do
     t.text "description"
     t.integer "price"
     t.string "location"
-    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "encrypted_password"
     t.text "description"
     t.string "first_name"
     t.string "last_name"
