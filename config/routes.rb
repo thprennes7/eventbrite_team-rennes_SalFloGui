f94875d9652c615bd1709d45351d9e3a064c0037
Salomé Marzin <salome.mrzn@gmail.com>
2019-02-13 17:49:59 +0100

    reset files devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 	root "events#index"
-  resources :users
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :events do
